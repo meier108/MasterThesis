9dd9714df2e56e4c3d212be80ff37e3aaa35251e
Meier Michael <meiermichael207@gmail.com>
2026-06-18 11:07:59 +0200

    changed mlp maxiterations to 500

--- models/mlp.py
+++ models/mlp.py
@@ -2,7 +2,7 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error
 
 class MLPModel:
-    def __init__(self, hidden_layer_sizes=(100,), activation='relu', solver='adam', max_iter=200):
+    def __init__(self, hidden_layer_sizes=(100,), activation='relu', solver='adam', max_iter=500):
         self.model = MLPRegressor(hidden_layer_sizes=hidden_layer_sizes, activation=activation, solver=solver, max_iter=max_iter)
 
     def fit(self, X_train, y_train):
